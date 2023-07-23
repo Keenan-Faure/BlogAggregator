278e4cd0b9423d4d92be94b7db42e3a496995859
Keenan-Faure <keenan.asterisk@gmail.com>
2023-07-23 20:19:32 +0200

    update

---
+++ sql/schema/007_shopify.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+CREATE TABLE shopify(
+    id UUID PRIMARY KEY,
+    title TEXT NOT NULL,
+    sku TEXT UNIQUE NOT NULL,
+    price DECIMAL NOT NULL,
+    qty INTEGER,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID,
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE shopify;
