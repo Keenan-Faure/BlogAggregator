a38b8509e2cf381a7ed037ee922af9aa97823aac
Keenan-Faure <keenan.asterisk@gmail.com>
2023-08-02 14:10:47 +0200

    login redirect

--- app/scripts/dom_creator.js
+++ app/scripts/dom_creator.js
@@ -5,26 +5,26 @@
  * @returns {void}
  */
 function createRegister(element) {
-    if (!registerExists()) {
-        let register_name = document.createElement("input");
-        register_name.id = "register.name";
-        register_name.type = "text";
-        register_name = appendStyleInput(register_name);
+	if (!registerExists()) {
+		let register_name = document.createElement("input");
+		register_name.id = "register.name";
+		register_name.type = "text";
+		register_name = appendStyleInput(register_name);
 
-        let register_tag = document.createElement("p");
-        register_tag.innerHTML = "Name: ";
+		let register_tag = document.createElement("p");
+		register_tag.innerHTML = "Name: ";
 
-        let register_button = document.createElement("button");
-        register_button = appendStyleBtn(register_button);
-        register_button.setAttribute("onclick", "registers()");
-        register_button.innerHTML = "Register";
+		let register_button = document.createElement("button");
+		register_button = appendStyleBtn(register_button);
+		register_button.setAttribute("onclick", "registers()");
+		register_button.innerHTML = "Register";
 
-        element.insertAdjacentElement("afterend", register_button);
-        element.insertAdjacentElement("afterend", document.createElement("br"));
-        element.insertAdjacentElement("afterend", document.createElement("br"));
-        element.insertAdjacentElement("afterend", register_name);
-        element.insertAdjacentElement("afterend", register_tag);
-    }
+		element.insertAdjacentElement("afterend", register_button);
+		element.insertAdjacentElement("afterend", document.createElement("br"));
+		element.insertAdjacentElement("afterend", document.createElement("br"));
+		element.insertAdjacentElement("afterend", register_name);
+		element.insertAdjacentElement("afterend", register_tag);
+	}
 }
 
 /**
@@ -33,11 +33,11 @@ function createRegister(element) {
  * @return {bool}
  */
 function registerExists() {
-    register = document.getElementById("register");
-    if (register != null) {
-        return true;
-    }
-    return false;
+	register = document.getElementById("register.name");
+	if (register != null) {
+		return true;
+	}
+	return false;
 }
 
 /**
@@ -47,8 +47,8 @@ function registerExists() {
  * @returns {HTMLElement}
  */
 function appendStyleInput(input_element) {
-    input_element.style.border = "1px solid black";
-    return input_element;
+	input_element.style.border = "1px solid black";
+	return input_element;
 }
 
 /**
@@ -58,10 +58,10 @@ function appendStyleInput(input_element) {
  * @returns {HTMLElement}
  */
 function appendStyleBtn(button_element) {
-    button_element.style.position = "relative";
-    button_element.style.left = "50%";
-    button_element.style.transform = "translate(-50%, 0%)";
-    return button_element;
+	button_element.style.position = "relative";
+	button_element.style.left = "50%";
+	button_element.style.transform = "translate(-50%, 0%)";
+	return button_element;
 }
 
 /**
@@ -70,28 +70,28 @@ function appendStyleBtn(button_element) {
  * @param {Number} responseCode Response code
  */
 function Message(message, responseCode) {
-    let button = document.createElement("button");
-    /** styles button */
-    button.style.zIndex = "5";
-    button.style.height = "25px";
-    button.style.bottom = "15px";
-    button.style.right = "15px";
-    button.style.position = "absolute";
-    button.style.border = "rgb(228 228 228)";
-    button.style.opacity = "0.9";
-    button.style.padding = "4px 12px";
-    button.style.borderRadius = "5px";
-    if (![200, 201].includes(responseCode)) {
-        button.style.background = "linear-gradient(rgb(209, 125, 125), red)";
-    } else {
-        button.style.background = "linear-gradient(rgb(28, 243, 233), blue)";
-    }
-    button.innerHTML = message;
+	let button = document.createElement("button");
+	/** styles button */
+	button.style.zIndex = "5";
+	button.style.height = "25px";
+	button.style.bottom = "15px";
+	button.style.right = "15px";
+	button.style.position = "absolute";
+	button.style.border = "rgb(228 228 228)";
+	button.style.opacity = "0.9";
+	button.style.padding = "4px 12px";
+	button.style.borderRadius = "5px";
+	if (![200, 201, 202].includes(responseCode)) {
+		button.style.background = "linear-gradient(rgb(209, 125, 125), red)";
+	} else {
+		button.style.background = "linear-gradient(rgb(28, 243, 233), blue)";
+	}
+	button.innerHTML = message;
 
-    referenceNode = document.body;
-    referenceNode.parentNode.insertBefore(button, referenceNode.nextSibling);
+	referenceNode = document.body;
+	referenceNode.parentNode.insertBefore(button, referenceNode.nextSibling);
 
-    setTimeout(() => {
-        button.remove();
-    }, 1500);
+	setTimeout(() => {
+		button.remove();
+	}, 1500);
 }
--- app/scripts/fetch.js
+++ app/scripts/fetch.js
@@ -1,5 +1,47 @@
 /**
- * Async function that fetches data from the API
+ * Async function that fetches data from the API (GET, DELETE)
+ *
+ * Headers & Params are arrays in key, value pairs
+ * ```json
+ * {
+ *  "key": "value"
+ * }
+ * ```
+ *
+ * @param {string} endpoint API Endpoint to request data from
+ * @param {string} method HTTP Option either `GET`, `POST`, `PUT`, `DELETE`
+ * @param {Object} headers Array of headers to be sent along with request
+ * @param {Object} params Array of params to be sent as query_params with the request
+ * @returns {any}
+ */
+const getEndpoint = async function (
+	endpoint,
+	method,
+	headers = {},
+	params = {}
+) {
+	let url = createURL(endpoint, params);
+	const resp = await fetch(url, {
+		method: method, // *GET, POST, PUT, DELETE, etc.
+		mode: "cors", // no-cors, *cors, same-origin
+		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: "include", // include, *same-origin, omit
+		headers: AppendHeaders(headers),
+		redirect: "follow", // manual, *follow, error
+		referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+	});
+	const json = await resp.json();
+	console.log(json);
+	console.log(endpoint);
+	let adHocMessage = EndpointAdHoc(endpoint, method, json);
+	if (adHocMessage != "undefined") {
+		Message(adHocMessage, resp.status);
+	}
+	return json;
+};
+
+/**
+ * Async function that fetches data from the API (POST, PUT)
  *
  * Headers & Params are arrays in key, value pairs
  * ```json
@@ -15,32 +57,51 @@
  * @param {Object} bodyData Object containing key-value pairs to be sent with POST request
  * @returns {any}
  */
-const fetchEndpoint = async function (
-    endpoint,
-    method,
-    headers = {},
-    params = {},
-    bodyData = {}
+const postEndpoint = async function (
+	endpoint,
+	method,
+	headers = {},
+	params = {},
+	bodyData = {}
 ) {
-    let url = createURL(endpoint, params);
-    const resp = await fetch(url, {
-        method: method, // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "include", // include, *same-origin, omit
-        headers: AppendHeaders(headers),
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(bodyData),
-    });
-    const json = await resp.json();
-    if (![200, 201].includes(resp.status)) {
-        Message(json.error, resp.status);
-    } else if ([200, 201].includes(resp.status)) {
-        Message("Success", resp.status);
-    }
-    return json;
+	let url = createURL(endpoint, params);
+	const resp = await fetch(url, {
+		method: method, // *GET, POST, PUT, DELETE, etc.
+		mode: "cors", // no-cors, *cors, same-origin
+		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+		credentials: "include", // include, *same-origin, omit
+		headers: AppendHeaders(headers),
+		redirect: "follow", // manual, *follow, error
+		referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+		body: JSON.stringify(bodyData),
+	});
+	const json = await resp.json();
+	console.log(json);
+	console.log(endpoint);
+	let adHocMessage = EndpointAdHoc(endpoint, method, json);
+	if (adHocMessage != "undefined") {
+		Message(adHocMessage, resp.status);
+	}
+	return json;
 };
+
+/**
+ *
+ * @param {string} endpoint endpoint that was queried
+ * @param {any} json response from API
+ * @returns {string}
+ */
+function EndpointAdHoc(endpoint, method, json) {
+	if ((endpoint == "users" && method) == "GET") {
+		setTimeout(() => {
+			window.location.href = "app/dashboard.html";
+		}, 500);
+		return "Success";
+	} else if (endpoint == "users" && method == "POST") {
+		document.getElementById("login.psw").value = json.api_key;
+		return "Pasted token inside password";
+	}
+}
 
 /**
  * Creates the request url
@@ -50,9 +111,9 @@ const fetchEndpoint = async function (
  * @returns {string}
  */
 function createURL(endpoint, params) {
-    arrayUrl = document.URL.split("/");
-    url = "http://" + arrayUrl[2] + "/v1/" + endpoint;
-    return appendParams(url, params);
+	arrayUrl = document.URL.split("/");
+	url = "http://" + arrayUrl[2] + "/v1/" + endpoint;
+	return appendParams(url, params);
 }
 
 /**
@@ -63,13 +124,13 @@ function createURL(endpoint, params) {
  * @returns {string}
  */
 function appendParams(url, params) {
-    if (Object.keys(params).length !== 0) {
-        url += "?";
-    }
-    for (const key in params) {
-        url += key + "=" + params[key];
-    }
-    return url;
+	if (Object.keys(params).length !== 0) {
+		url += "?";
+	}
+	for (const key in params) {
+		url += key + "=" + params[key];
+	}
+	return url;
 }
 
 /**
@@ -79,9 +140,9 @@ function appendParams(url, params) {
  * @return {Object}
  */
 function AppendHeaders(headers) {
-    headers["Access-Control-Allow-Origin"] = "*";
-    headers["Content-Type"] = "application/json";
-    return headers;
+	headers["Access-Control-Allow-Origin"] = "*";
+	headers["Content-Type"] = "application/json";
+	return headers;
 }
 
 // sessionStorage.setItem("lastname", "Smith");
