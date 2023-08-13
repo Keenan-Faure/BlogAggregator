b3716cd609d84449938ae1eebbc5722d6fe23ec1
keenan-faure <keenan.asterisk@gmail.com>
2023-08-13 15:20:02 +0200

    update

--- stop.sh
+++ stop.sh
@@ -8,5 +8,14 @@ docker stop blog_aggregator
 docker rm blogaggregator-database-1
 docker rm blog_aggregator
 
-#removes containers
-docker rmi $(docker images 'blogaggregator-web' -a -q) -f
+#removes images
+ 
+RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null)
+
+if [ $(docker image inspect blogaggregator-web) == [] ]; then
+  echo "'blogaggregator-web' does not exist."
+else
+  echo "Does exist"
+fi
+
+#docker rmi $(docker images 'blogaggregator-web' -a -q) -f
